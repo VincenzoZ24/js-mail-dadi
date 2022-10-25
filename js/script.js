6dee8840ceae8c6c7cf3b49afa83562fabafc40b
VincenzoZ24 <zitov287@gmail.com>
2022-10-25 19:56:40 +0200

    collegata interfaccia

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 const mail = ["zitov287@gmail.com", "pinco@gmail.com", "pietrozitoinf@gmail.com", "deianaveronica1@gmail.com"];
-let utente = prompt("scrivi la tua e-mail");
-if (mail.includes(utente)) {
-    alert("il gioco dei dadi ha inizio");
+let utente = document.getElementById("email_utente");
+// let form = document.querySelector("form");
+let submit = document.getElementById("invia");
 
-    let numPlayer = Math.floor(Math.random() * 6) + 1;
-    alert("il tuo numero è: " + numPlayer);
-    let numComputer = Math.floor(Math.random() * 6) + 1;
-    alert(" il numero del tuo avversario é: " + numComputer);
-    if (numPlayer > numComputer) {
-        alert("HAI VINTO");
+
+submit.addEventListener("click", function play () {
+    
+    if ((mail).includes(utente.value)) {
+        alert("il gioco dei dadi ha inizio");
+    
+        let numPlayer = Math.floor(Math.random() * 6) + 1;
+        document.querySelector(".player").innerHTML = `il tuo numero è: ${numPlayer}`  ;
+        let numComputer = Math.floor(Math.random() * 6) + 1;
+        document.querySelector(".player2").innerHTML = `il del tuo avversario è: ${numComputer}`  ;
+        if (numPlayer > numComputer) {
+           document.querySelector(".risultato").innerHTML = "HAI VINTO"
+        } else if (numPlayer < numComputer) {
+            document.querySelector(".risultato").innerHTML = "HAI PERSO";
+        }else{
+            document.querySelector(".risultato").innerHTML = "PAREGGIO";
+        };
+    
     } else {
-        alert("HAI PERSO");
+        alert("non puoi iniziare in gioco dei dadi, l' e-mail non è corretta");
     };
-
-} else {
-    alert("non puoi iniziare in gioco dei dadi, l' e-mail non è corretta");
-};
+    
+});
