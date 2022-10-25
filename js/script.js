8c832aac5b67366f9b4195ef68c2598809d36987
VincenzoZ24 <zitov287@gmail.com>
2022-10-25 15:56:51 +0200

    imposto gioco dei dadi dopo che l'email è corretta

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const mail = ["zitov287@gmail.com", "pinco@gmail.com", "pietro24@gmail.com" ];
 let utente = prompt("scrivi la tua e-mail");
  if (mail.includes(utente)) {
-    console.log("accesso consentito");
+    alert("il gioco dei dadi ha inizio");
+
+    let numPlayer = Math.floor(Math.random() * 6) + 1;
+    alert (numPlayer);
+    let numComputer = 
+
  }else{
     alert("accesso negato");
- }
+ };
