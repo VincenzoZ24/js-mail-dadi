28f935063832eac9d34630529917e938c949b7c9
VincenzoZ24 <zitov287@gmail.com>
2022-10-25 16:22:48 +0200

    agg.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-const mail = ["zitov287@gmail.com", "pinco@gmail.com", "pietrozitoinf@gmail.com", "deianaveronica1@gmail.com" ];
+const mail = ["zitov287@gmail.com", "pinco@gmail.com", "pietrozitoinf@gmail.com", "deianaveronica1@gmail.com"];
 let utente = prompt("scrivi la tua e-mail");
- if (mail.includes(utente)) {
+if (mail.includes(utente)) {
     alert("il gioco dei dadi ha inizio");
 
     let numPlayer = Math.floor(Math.random() * 6) + 1;
-    alert ( "il tuo numero è: " + numPlayer);
-    let numComputer =  Math.floor(Math.random() * 6) + 1;
-    alert( " il numero del tuo avversario é: " + numComputer);
-    if( numPlayer > numComputer){
+    alert("il tuo numero è: " + numPlayer);
+    let numComputer = Math.floor(Math.random() * 6) + 1;
+    alert(" il numero del tuo avversario é: " + numComputer);
+    if (numPlayer > numComputer) {
         alert("HAI VINTO");
-    }else{
-        alert ("HAI PERSO");
+    } else {
+        alert("HAI PERSO");
     };
 
- }else{
+} else {
     alert("non puoi iniziare in gioco dei dadi, l' e-mail non è corretta");
- };
+};
